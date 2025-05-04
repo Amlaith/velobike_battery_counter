8d2f47bbf0361f079711646ab6deca9946f1ff84
Amlaith <burjooi@gmail.com>
2025-05-05 00:09:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ from image_processing import remove_green, bike_screenshot_to_string
 uploaded_files = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True, type=["jpg", "jpeg", "png"]
 )
+
+text = ""
+
 for uploaded_file in uploaded_files:
     imagefile = io.BytesIO(uploaded_file.read())
-    im = Image.open(imagefile)
-    # Do something with im
-    st.image(im)
+    img = Image.open(imagefile)
+    # st.image(im)
+    img = remove_green(img)
+    text += bike_screenshot_to_string(img)
+
+st.write(text)
+
+
 
